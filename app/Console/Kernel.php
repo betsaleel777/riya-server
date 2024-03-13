cdfb1bb6c645045b446a8e29333f2718b1063247
Ahoussou Jean-Chris Arnaud <shin.ahoussou@gmail.com>
2024-03-13 00:05:11 +0000

    tache de serveur corrigée pour cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->call(new CreateRent)->everyThreeMinutes();
+        $schedule->call(new CreateRent);
     }
 
     /**
