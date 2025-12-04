694a3a4552e7a1381347f5da01f48110fe22a39a
Syitechdev <dev@syitech-group.tech>
2025-12-04 11:48:20 +0000

    correction de configuration server image media

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,6 +34,8 @@ return [
             'driver' => 'local',
             'root' => public_path('media'),
             'url' => env('APP_URL') . '/media',
+            'visibility' => 'public',
+            'throw' => false,
         ],
 
         'local' => [
